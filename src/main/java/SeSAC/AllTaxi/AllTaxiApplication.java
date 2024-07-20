ec9f7d7e6a35af2205c7aaf9110cdb090ae36c18
uuujini <youjin010402@naver.com>
2024-07-20 23:40:19 +0900

    Initial commit

---
+++ src/main/java/SeSAC/AllTaxi/AllTaxiApplication.java
@@ -0,0 +1,13 @@
+package SeSAC.AllTaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllTaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SeSAC/AllTaxi/AllTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package SeSAC.AllTaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
