b352df24f66ff6f729dd4c48ed6f4d06719a43a8
uuujini <youjin010402@naver.com>
2024-07-22 00:58:08 +0900

    fix: Request 엔티티 Dto 분리

--- src/main/java/com/sesac/alltaxi/domain/Request.java
+++ src/main/java/com/sesac/alltaxi/domain/Request.java
@@ -5,8 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Request {
 
     @Id
@@ -21,53 +25,4 @@ public class Request {
 
     @ManyToOne
     private Driver driver;
-
-    // Getter & Setter 메서드 추가
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPickupLocation() {
-        return pickupLocation;
-    }
-
-    public void setPickupLocation(String pickupLocation) {
-        this.pickupLocation = pickupLocation;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Driver getDriver() {
-        return driver;
-    }
-
-    public void setDriver(Driver driver) {
-        this.driver = driver;
-    }
 }
