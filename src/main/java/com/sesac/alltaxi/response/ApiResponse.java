2a8cb1e4eac69e3ecf6a38756854c0da9926f215
uuujini <youjin010402@naver.com>
2024-08-01 14:19:36 +0900

    feat: 상태코드 응답 추가

--- src/main/java/com/sesac/alltaxi/response/ApiResponse.java
+++ src/main/java/com/sesac/alltaxi/response/ApiResponse.java
@@ -5,21 +5,29 @@ import lombok.Getter;
 @Getter
 public class ApiResponse<T> {
 
+    private final T requestId;
     private final int status;
-
     private final String message;
 
-    private ApiResponse(int status, String message) {
+    private ApiResponse(T requestId, int status, String message) {
         this.status = status;
         this.message = message;
+        this.requestId = requestId;
     }
 
     public static ApiResponse<Void> ok() {
         return ok(null);
     }
 
-    public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<>(200, "OK");
+    public static <T> ApiResponse<T> ok(T requestId) {
+        return new ApiResponse<>(requestId, 200, "OK");
     }
 
+    public static <T> ApiResponse<T> created(T requestId) {
+        return new ApiResponse<>(requestId, 201, "Created");
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message) {
+        return new ApiResponse<>(null, status, message);
+    }
 }
